4f22025dccb9413e00d940c9437ed230730b039d
Molunerfinn <marksz@teamsz.xyz>
2018-09-21 15:58:15 +0800

    Added: copy templates

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 A [picgo](https://github.com/PicGo/PicGo-Core) plugin for auto copying url to clipboard after uploading.
 
+> CLI Only.
+
 ![](https://raw.githubusercontent.com/Molunerfinn/test/master/picgo/picgo-plugin-autocopy.gif)
+
+**It's useless for the electron version of [PicGo](https://github.com/Molunerfinn/PicGo) since it already has this feature. It's useful when you are using picgo in CLI.**
 
 ## Installation
 
@@ -10,6 +14,33 @@ A [picgo](https://github.com/PicGo/PicGo-Core) plugin for auto copying url to cl
 picgo install picgo-plugin-autocopy
 ```
 
-## Notice
+## Configuration
 
-It's useless for the electron version of [PicGo](https://github.com/Molunerfinn/PicGo) since it already has this feature. It's useful when you are using picgo in CLI.
+`picgo-plugin-autocopy` supports 5 kinds of url types:
+
+- Markdown
+- URL (default)
+- HTML
+- UBB
+- Custom
+
+To change the default type of url, please run:
+
+```bash
+picgo set pluin autocopy
+```
+
+**Custom** means you can create your own types of url for copying. Just place the `$url` to your own string.
+
+For example:
+
+```
+<a href="$url"><img src="$url"></a>
+```
+
+And it will be coverted to:
+
+```
+<a href="https://xxx.jpg"><img src="https://xxx.jpg"></a>
+```
+
--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,63 @@ import os from 'os'
 import clipboardy from 'clipboardy'
 
 export = (ctx: picgo) => {
+  const config = (ctx: picgo) => {
+    let userConfig = ctx.getConfig('picgo-plugin-autocopy')
+    if (!userConfig) {
+      userConfig = {}
+    }
+    const config = [
+      {
+        name: 'template',
+        type: 'list',
+        choices: ['Markdown', 'HTML', 'URL', 'UBB', 'Custom'],
+        default: 'URL'
+      },
+      {
+        name: 'customLink',
+        type: 'input',
+        message: 'Please place the $url to where you want.',
+        when (answer) {
+          return answer.template === 'Custom'
+        },
+        default: '$url'
+      }
+    ]
+    return config
+  }
+
+  const handleConfig = async (ctx: picgo) => {
+    const prompts = config(ctx)
+    const answer = await ctx.cmd.inquirer.prompt(prompts)
+    ctx.saveConfig({
+      'picgo-plugin-autocopy': answer
+    })
+  }
+
   const register = () => {
     ctx.on('finished', (msg: picgo) => {
+      const customLink = ctx.getConfig('picgo-plugin-autocopy.customLink') || '$url'
+      const pasteType = ctx.getConfig('picgo-plugin-autocopy.template') || 'URL'
+      const tpl = url => {
+        return {
+          'Markdown': `![](${url})`,
+          'HTML': `<img src="${url}"/>`,
+          'URL': url,
+          'UBB': `[IMG]${url}[/IMG]`,
+          'Custom': customLink.replace(/\$url/g, url)
+        }
+      }
       let urls = ''
       let newline = os.platform() === 'win32' ? '\r\n' : '\n'
       msg.output.forEach(item => {
-        urls += item.imgUrl + newline
+        urls += tpl(item.imgUrl)[pasteType] + newline
       })
       clipboardy.writeSync(urls)
     })
   }
   return {
-    register
+    register,
+    config,
+    handleConfig
   }
 }
